a9614925b98b115ca48527689d2aea1005893792
AndyHou <andyhou965@gmail.com>
2024-11-26 15:02:43 +0400

    implement the converstation and llm

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import (
+    OpenAIEmbeddings,
+    HuggingFaceInstructEmbeddings,
+)
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
+from langchain_community.llms import HuggingFaceHub
 
 
 def get_pdf_text(pdf_docs):
@@ -23,12 +33,54 @@ def get_text_chunks(text):
     return chunks
 
 
+def get_vector_store(chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vector_store):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=vector_store.as_retriever(), memory=memory
+    )
+
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({"question": user_question})
+    st.session_state.chat_history = response["chat_history"]
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(
+                user_template.replace("{{MSG}}", message.content),
+                unsafe_allow_html=True,
+            )
+        else:
+            st.write(
+                bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
+            )
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Docs", page_icon="🗂️", layout="wide")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with Docs 🗂️")
-    st.text_input("Ask a question about your documents:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -45,6 +97,10 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
 
                 # create the vector store
+                vector_store = get_vector_store(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 
 if __name__ == "__main__":
